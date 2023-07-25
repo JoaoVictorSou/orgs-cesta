cf7c7cfcda95860846762863eab6c89e67f53049
JoaoVictorSou <soulima.joao@gmail.com>
2023-07-24 21:42:12 -0300

    feat: purchase image and title styling

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Purchase from './src/screen/Purchase';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Purchase></Purchase>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar />
+      <Purchase />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screen/Purchase.jsx
+++ src/screen/Purchase.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, Image, StyleSheet, Dimensions, View } from 'react-native'
+
+import top from '../../assets/topo.png'
+
+const width = Dimensions.get('screen').width
 
 let Purchase = () => {
-    return <Text>Cesta 1</Text>
+    return (
+        <> 
+            <Image source = {top} style = {styles.top} />
+            <Text style = {styles.title}>Detalhes da cesta</Text>
+
+            <View style = {styles.purchase}>
+                <Text style = {styles.purchase_name}>Cesta de Verduras</Text>
+                <Text>Jenny Jack Farm</Text>
+                <Text>
+                    Uma cesta com produtos selecionados
+                    cuidadosamente da fazenda direto para
+                    sua cozinha.
+                </Text>
+            </View>
+        </>
+    )
 }
+
+const styles = StyleSheet.create({
+    top: {
+        width: "100%",
+        height: (578/768) * width
+    },
+    title: {
+        fontSize: width/20,
+        width: '100%',
+        position: 'absolute',
+        textAlign: 'center',
+        lineHeight: width/20,
+        color: 'white',
+        fontWeight: 'bold',
+        padding: width/15
+    },
+    purchase: {
+        paddingVertical: width/40,
+        paddingHorizontal: width/25
+    },
+    purchase_name: {
+        fontSize: width/15,
+        lineHeight: width/8,
+        color: '#464646',
+        fontWeight: 'bold'
+    }
+})
 
 export default Purchase
